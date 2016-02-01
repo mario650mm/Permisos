22bef520706539c644b022d2215da8bc8298f6eb
mario650mm <mario650m@gmail.com>
2016-01-31 19:27:27 -0600

    .gitignore is now working

---
+++ Permisos/app/Policies/PostPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Post;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine if the given post can be updated by the user.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return bool
+     */
+
+    public function update(User $user, Post $post)
+    {
+        return $user->id === $post->user_id;
+    }
+}
